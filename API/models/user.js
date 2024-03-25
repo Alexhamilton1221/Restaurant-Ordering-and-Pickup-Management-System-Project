b3c9318c947c50ca468fbda4f770c2b878fd935c
Alexander Hamilton <alexhamilton2001@gmail.com>
2024-03-24 23:36:00 -0600

    Remove hash.

--- API/models/user.js
+++ API/models/user.js
@@ -19,16 +19,6 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
-// Hash the password before saving the user
-userSchema.pre("save", async function (next) {
-  const user = this;
-  if (!user.isModified("password")) {
-    return next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-  next();
-});
 
 // Method to compare password with stored password hash
 userSchema.methods.comparePassword = async function (password) {
